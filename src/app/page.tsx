e0517c92b1c6673f2c8c8542e76f38dec3a130d0
Prakhar Gupta <prakhar897@gmail.com>
2024-07-14 19:32:22 +0530

    x

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,9 +15,9 @@ export default function Home() {
     const hours = Math.floor(time / 60);
     const minutes = time % 60;
     const timestamp = `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, '0')}-${String(date.getDate()).padStart(2, '0')} ${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}`;
-
+    console.log(timestamp);
     try {
-      const response = await fetch('https://master-node-backend.onrender.com/news-in-motion/get-snapshots', {
+      const response = await fetch('/news-in-motion/get-snapshots', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -28,17 +28,11 @@ export default function Home() {
         }),
       });
       const data = await response.json();
+      console.log(data);
+
       setImages(data);
     } catch (error) {
       console.error('Failed to fetch images from API:', error);
-      // Use placeholder image from placeholder.co
-      const placeholderImage: ImageData = {
-        site: "Placeholder",
-        image_base64: "https://via.placeholder.com/1920x1080",
-      };
-
-      const imagesArray = new Array(20).fill(placeholderImage);
-      setImages(imagesArray);   
     }
     setSelectedTime(time);
   };
@@ -52,7 +46,7 @@ export default function Home() {
       <div className="grid grid-cols-2">
         {images.map((item, index) => (
           <div key={index} className="bg-gray-200 border border-gray-300">
-            <img src={item.image_base64} alt="Fetched" />
+            <img src={`data:image/png;base64,${item.image_base64}`} alt={`Front page of ${item.site}`} />
           </div>
         ))}
       </div>
--- src/next.config.mjs
+++ src/next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/news-in-motion/get-snapshots',
+        destination: 'https://master-node-backend.onrender.com/news-in-motion/get-snapshots',
+      },
+    ];
+  },
+};
 
 export default nextConfig;
